85ecd032e60097c32d35d5739c4c50f01211976e
Anibal Benalcazar <abenalcazar@KRUGER.COM.EC>
2018-07-12 15:43:32 -0500

    instalacion angular material, animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Aplicacion con angular 6';
+  autor = 'Anibal';
+  version = '1.0.0';
 }
