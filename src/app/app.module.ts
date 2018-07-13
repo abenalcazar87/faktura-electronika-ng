2824b7bd188369464a0815648c2d45bead2784d4
Anibal Benalcazar <abenalcazar@KRUGER.COM.EC>
2018-07-13 15:57:21 -0500

    ejemplo de material, anadiendo nuevos modulos en appmodule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatCardModule, MatMenuModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -16,6 +16,8 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
+    MatCardModule,
+    MatMenuModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
