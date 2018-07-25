8407e4daffabc609b15beb4217469b047a413c8d
Anibal <anibal_benalcazar@yahoo.es>
2018-07-25 00:47:38 -0500

    implementar dashboard estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,92 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Type } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule, Title }  from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { CovalentCommonModule } from '@covalent/core/common';
+import { CovalentLayoutModule } from '@covalent/core/layout';
+import { CovalentMediaModule } from '@covalent/core/media';
+import { CovalentLoadingModule } from '@covalent/core/loading';
+
+import { CovalentHttpModule, IHttpInterceptor } from '@covalent/http';
+
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+import { appRoutes } from './app.routes';
 
 import { AppComponent } from './app.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { RequestInterceptor } from '../config/interceptors/request.interceptor';
+import { MOCK_API } from '../config/api.config';
+
+import { USER_PROVIDER, USERS_API } from './users';
+import { MainComponent } from './main.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const httpInterceptorProviders: Type<any>[] = [
+  RequestInterceptor,
+];
+
+export function getAPI(): string {
+  return MOCK_API;
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
-  ],
+    MainComponent,
+    LoginComponent,
+    DashboardComponent,
+  ], // directives, components, and pipes owned by this NgModule
   imports: [
+    // angular modules
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
+    FormsModule,
+    HttpClientModule,
+    HttpModule,
+    // material modules
     MatButtonModule,
-    MatSidenavModule,
+    MatCardModule,
     MatIconModule,
-    MatListModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    MatListModule,
+    MatDividerModule,
+    MatInputModule,
+    MatToolbarModule,
+    // covalent modules
+    CovalentCommonModule,
+    CovalentLayoutModule,
+    CovalentMediaModule,
+    CovalentLoadingModule,
+    CovalentHttpModule.forRoot({
+      interceptors: [{
+        interceptor: RequestInterceptor, paths: ['**'],
+      }],
+    }),
+    // external modules
+    NgxChartsModule,
+    // routes
+    appRoutes,
+  ], // modules needed to run this module
+  providers: [
+    httpInterceptorProviders,
+    Title, {
+      provide: USERS_API, useFactory: getAPI,
+    }, USER_PROVIDER,
+  ], // additional providers needed for this module
+  entryComponents: [ ],
+  bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
